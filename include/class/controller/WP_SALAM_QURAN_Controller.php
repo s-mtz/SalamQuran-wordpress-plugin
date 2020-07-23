fd725c68a73a78f3766847fa181bd58550a97aa0
S.Morteza Hakimzade <s.mtz.hakim@gmail.com>
2020-07-23 19:30:18 +0430

    add the widget into controller

--- include/class/controller/WP_SALAM_QURAN_Controller.php
+++ include/class/controller/WP_SALAM_QURAN_Controller.php
@@ -1,21 +1,39 @@
 <?php
 
-include SALAM_QURAN_INC_DIR . "class/model/WP_SALAM_QURAN_Model.php";
-include SALAM_QURAN_INC_DIR . "class/view/WP_SALAM_QURAN_View.php";
+use Automattic\WooCommerce\Blocks\Assets\Api;
+
+include SALAM_QURAN_INC_DIR_CTRL . "WP_SALAM_QURAN_Widget.php";
+include SALAM_QURAN_INC_DIR . "class/Model/WP_SALAM_QURAN_Api.php";
+
 
 class WP_SALAM_QURAN_Controller
 {
-    private $model;
-    private $view;
+    private $api;
+
     public function __construct()
     {
-        $this->model = new WP_SALAM_QURAN_Model();
-        $this->view = new WP_SALAM_QURAN_View();
+        $this->api = new WP_SALAM_QURAN_Api();
+
+        add_shortcode('aye_rooz', [$this, 'day_aye']);
+        add_shortcode('aye_rand', [$this, 'rand_aye']);
+
+        add_action('widgets_init', [$this, 'register_salam_quran']);
+        // $this->view = new WP_SALAM_QURAN_View();
     }
 
-    public function test()
+    function register_salam_quran()
     {
+        register_widget('Salam_Quran_Widget');
+    }
 
-        $this->view->test("Test");
+    public function day_aye()
+    {
+        $data = $this->api->day_aye();
+        return $data;
+    }
+
+    public function rand_aye()
+    {
+        return $this->api->rand_aye();
     }
 }
