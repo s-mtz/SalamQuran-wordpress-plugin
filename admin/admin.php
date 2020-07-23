5e491233be4081233e16232cc13a99bf834a7e38
S.Morteza Hakimzade <s.mtz.hakim@gmail.com>
2020-07-23 19:36:54 +0430

    make the admin and sub menue

--- admin/admin.php
+++ admin/admin.php
@@ -1,16 +1,27 @@
 <?php
 add_action('admin_menu', 'SalamQuran_admin_menu');
 
-function SalamQuran_admin_menu(){
+function SalamQuran_admin_menu()
+{
+    $main = add_menu_page(
+        __('SALAM_QURAN', 'SalamQuran-wp'), // page title
+        __('SALAM_QURAN', 'SalamQuran-wp'), // menue title
+        'manage_options',                   // capability
+        'SalamQuran_dashboard',             // menu slug
+        'SalamQuran_dashboard_page',        // callable function
+        SALAM_QURAN_IMG_URL . "logo.svg"    // icon url
+    );
 
-	$main = add_menu_page(__('SALAM_QURAN', 'SalamQuran-wp'), __('SALAM_QURAN', 'SalamQuran-wp'), 'manage_options', 'SalamQuran_dashboard', 'SalamQuran_dashboard_page', SALAM_QURAN_IMG_URL."logo.svg");
-
-	$main_sub = add_submenu_page('SalamQuran_dashboard', __('SALAM_QURAN', 'SalamQuran-wp'), __('SALAM_QURAN', 'SalamQuran-wp'), 'manage_options', 'SalamQuran_dashboard');
-
+    $main_sub = add_submenu_page(
+        'SalamQuran_dashboard',
+        __('SALAM_QURAN', 'SalamQuran-wp'),
+        __('SALAM_QURAN', 'SalamQuran-wp'),
+        'manage_options',
+        'SalamQuran_dashboard'
+    );
 }
 
-function SalamQuran_dashboard_page ()
+function SalamQuran_dashboard_page()
 {
-    // global $BTC;
-    require SALAM_QURAN_TPL_DIR.'html-admin-main.php';
+    require SALAM_QURAN_TPL_DIR . 'html-admin-main.php';
 }
